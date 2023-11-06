f8f508e7c4911e7080e8ec3e202de54bfd4971c1
Geleia <luizcanf@gmail.com>
2023-11-06 18:03:14 -0300

    JS separado no servidor

---
+++ Aula25/serverNode/server.js
@@ -0,0 +1,18 @@
+const port = 8080;
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.get("/", (request, response) => {
+    response.render("soma4");
+});
+
+app.get("/soma", (request, response) => {
+    let soma = parseFloat(request.query.num1) + parseFloat(request.query.num2)
+    response.render("soma4", {result: soma});
+});
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando. http://localhost:${port}`);
+});
